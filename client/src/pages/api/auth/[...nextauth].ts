1179f86bb0f940cdab32aa32b3d6fe175f6bb73a
ReasonJun <117330708+YouJun-IWON@users.noreply.github.com>
2023-10-29 14:31:37 +0900

    feat: test10

--- client/src/pages/api/auth/[...nextauth].ts
+++ client/src/pages/api/auth/[...nextauth].ts
@@ -1,9 +1,7 @@
 import NextAuth, { NextAuthOptions } from 'next-auth';
 import { MoralisNextAuthProvider } from '@moralisweb3/next';
 import { PrismaAdapter } from '@auth/prisma-adapter';
-import { PrismaClient } from '@prisma/client';
-
-const prisma = new PrismaClient();
+import prisma from '@/helpers/prismadb';
 
 export const authOptions: NextAuthOptions = {
   adapter: PrismaAdapter(prisma),
