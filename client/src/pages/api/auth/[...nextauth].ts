312eed1dac2747195a55e2161fff3eacf4d31157
ReasonJun <117330708+YouJun-IWON@users.noreply.github.com>
2023-10-30 03:14:48 +0900

    feat: 3

--- client/src/pages/api/auth/[...nextauth].ts
+++ client/src/pages/api/auth/[...nextauth].ts
@@ -1,60 +1,19 @@
-import NextAuth, { NextAuthOptions } from 'next-auth';
+import NextAuth from 'next-auth';
 import { MoralisNextAuthProvider } from '@moralisweb3/next';
-import { PrismaAdapter } from '@auth/prisma-adapter';
-import { PrismaClient } from '@prisma/client';
 
-const prisma = new PrismaClient();
-
-export const authOptions: NextAuthOptions = {
-  adapter: PrismaAdapter(prisma),
+export default NextAuth({
   providers: [MoralisNextAuthProvider()],
   // adding user info to the user session object
-  session: {
-    strategy: 'jwt',
-  },
-  jwt: {
-    maxAge: 30 * 24 * 60 * 60, // 30 days
-  },
-  pages: {
-    signIn: '/auth/login',
-  },
   callbacks: {
-    async jwt({ token, user }: any) {
-      token.user = user;
-
+    async jwt({ token, user }) {
       if (user) {
-        const profile = await prisma.user.findUnique({
-          where: {
-            account: token.user.address,
-          },
-        });
-
-        if (!profile) {
-          // If the user doesn't exist, create a new entry
-          await prisma.user.create({
-            data: {
-              account: token.user.address,
-            },
-          });
-
-          const profile = await prisma.user.findUnique({
-            where: {
-              account: token.user.address,
-            },
-          });
-          token.user = { ...token, ...user, profile: { ...profile } };
-          return token;
-        }
-        token.user = { ...token, ...user, profile: { ...profile } };
-        return token;
+        token.user = user;
       }
       return token;
     },
-    async session({ session, token }: any) {
+    async session({ session, token }) {
       (session as { user: unknown }).user = token.user;
       return session;
     },
   },
-};
-
-export default NextAuth(authOptions);
+});
