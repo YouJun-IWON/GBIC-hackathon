6a7f6f098cfc649d3add35b0fdc9894b219613be
ReasonJun <117330708+YouJun-IWON@users.noreply.github.com>
2023-10-30 16:35:26 +0900

    feat: ㅁ

--- client/src/pages/_document.tsx
+++ client/src/pages/_document.tsx
@@ -6,10 +6,6 @@ export default function Document() {
   return (
     <Html lang='en'>
       <Head>
-        <meta
-          http-equiv='Content-Security-Policy'
-          content='upgrade-insecure-requests'
-        />
         <script
           type='text/javascript'
           src={`//dapi.kakao.com/v2/maps/sdk.js?appkey=${process.env.NEXT_PUBLIC_KAKAOMAP_KEY}&libraries=services,clusterer`}
